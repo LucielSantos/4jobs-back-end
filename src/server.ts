05d0e276b3d4d71f18fed07c35c52112c037c2e6
Luciel Santos <luciel.santos@ubistart.com>
2021-04-10 01:51:15 -0300

    config typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import 'reflect-metadata'
 import express from 'express'
-
 import cors from 'cors'
+import './database'
 
 const app = express()
 
@@ -10,4 +11,6 @@ app.get('/', (req, res) => {
   return res.json({ message: 'Hello world' })
 })
 
-app.listen(process.env.PORT || 3333)
+app.listen(process.env.PORT || 3333, (err) => {
+  !err && console.log('Server is running! \\o/')
+})
