8c74639b2302bbe10e9d52abdfba05a20b62f72e
Luciel Santos <luciel.santos@ubistart.com>
2021-04-13 21:27:44 -0300

    create migration to create company table

---
+++ src/database/migrations/1618357270543-CreateCompanyTable.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateCompanyTable1618357270543 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: 'companies',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'responsible',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'marketSegment',
+            type: 'varchar',
+          },
+          {
+            name: 'cnpj',
+            type: 'varchar',
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'profileImage',
+            type: 'varchar',
+          },
+        ],
+      }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('companies')
+  }
+}
