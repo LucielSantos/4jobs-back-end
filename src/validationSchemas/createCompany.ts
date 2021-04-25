029dec169f0c5ff58b719b49257c2b1e605dca0f
Luciel Santos <luciel.santos@ubistart.com>
2021-04-25 17:51:57 -0300

    email validation

--- src/validationSchemas/createCompany.ts
+++ src/validationSchemas/createCompany.ts
@@ -1,12 +1,11 @@
 import * as Yup from 'yup'
 import { passwordMatches } from './constants'
-import { requiredMessage, invalidPassword, matchPasswordError } from './messages'
+import { requiredMessage, invalidPassword, invalidEmail } from './messages'
 
 export const createCompanyValidationSchema = Yup.object().shape({
   profileImage: Yup.string().required(requiredMessage),
   name: Yup.string().required(requiredMessage),
-  // TODO: validate email
-  email: Yup.string().required(requiredMessage),
+  email: Yup.string().email(invalidEmail).required(requiredMessage),
   responsible: Yup.string().required(requiredMessage),
   cnpj: Yup.string().required(requiredMessage),
   phone: Yup.string().required(requiredMessage),
