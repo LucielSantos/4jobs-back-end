bcc00cdcbde82586c135b34d8e5b791f4fc3b382
Luciel Santos <luciel.santos@ubistart.com>
2021-06-11 00:27:30 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,6 @@ import { router } from './routes'
 
 const app = express()
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
-  next()
-})
-
-app.all('*', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-})
-
 app.use(cors())
 
 app.use(express.json({ limit: '100mb' }))
