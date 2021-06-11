61c29f76c508e4436e1c558032c0db859421cc74
Luciel Santos <luciel.santos@ubistart.com>
2021-06-10 23:48:59 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ const allowCrossDomain = function (req: Request, res: Response, next: NextFuncti
 
 app.use(allowCrossDomain)
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json({ limit: '100mb' }))
 
