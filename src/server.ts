f1502d4920aaf8cc31ba7ef3ea178e6cd43cb772
Luciel Santos <luciel.santos@ubistart.com>
2021-06-10 23:47:10 -0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import 'reflect-metadata'
 
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import './database'
 import { router } from './routes'
 
 const app = express()
+
+const allowCrossDomain = function (req: Request, res: Response, next: NextFunction) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+  // intercept OPTIONS method
+  // eslint-disable-next-line
+  if (req.method == 'OPTIONS') {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+
+app.use(allowCrossDomain)
 
 app.use(cors())
 
